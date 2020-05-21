7edcd1da957372242f0ec0e378f805263630a86d
chrisipeters <chris.peters@menoko.com.au>
2020-05-21 12:12:04 +1000

    fixed meta data for dm command

--- sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_dm.cs
+++ sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_dm.cs
@@ -8,7 +8,7 @@ namespace sctm.services.discordBot.Commands.Messages
     public partial class MessageCommands
     {
         [Command("dm")] // let's define this method as a command
-        [Description("Example ping command")] // this will be displayed to tell users what this command does when they invoke help
+        [Description("Request a DM from ChrispyKoala - this is ideal for commands you don't want to make in public")] // this will be displayed to tell users what this command does when they invoke help
         [Aliases("chat","pm")] // alternative names for the command
         public async Task AddCommands_DM(CommandContext ctx)
         {
