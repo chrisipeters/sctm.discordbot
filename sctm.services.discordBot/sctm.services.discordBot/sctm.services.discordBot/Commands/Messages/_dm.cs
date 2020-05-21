cedceb106b6ef2002c2208afcaa17e202074ba90
chrisipeters <chris.peters@menoko.com.au>
2020-05-21 12:09:07 +1000

    added dm command

---
+++ sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_dm.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace sctm.services.discordBot.Commands.Messages
+{
+    public partial class MessageCommands
+    {
+        [Command("dm")] // let's define this method as a command
+        [Description("Example ping command")] // this will be displayed to tell users what this command does when they invoke help
+        [Aliases("chat","pm")] // alternative names for the command
+        public async Task AddCommands_DM(CommandContext ctx)
+        {
+            var _dmChannel = await ctx.CommandsNext.Client.CreateDmAsync(ctx.User);
+            await _dmChannel.SendMessageAsync("Hi there! What can I do for you today?");
+        }
+    }
+}
