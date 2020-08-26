b6d881258b8af46e86314f89f9881caf836b88a8
chrisipeters <chris.peters@menoko.com.au>
2020-08-26 20:50:46 +1000

    added ships

---
+++ sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_ships.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using sctm.connectors.rsi.models;
+using Serilog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sctm.services.discordBot.Commands.Messages
+{
+    public partial class MessageCommands
+    {
+        [Command("ships")] // let's define this method as a command
+        [Description("Get information about a ship in Star Citizen")] // this will be displayed to tell users what this command does when they invoke help
+        [Aliases("ship")] // alternative names for the command
+        public async Task AddCommands_Ship(CommandContext ctx, [Description("Search term")] params string[] args)
+        {
+            var _logAction = "AddCommands_Ship";
+            await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":mag:"));
+
+            var _client = await _services.GetSCTMClient();
+            if (_client == null)
+            {
+                Log.Error("{logAction}: unable to get SCTM client", _logAction);
+                await ctx.Message.DeleteOwnReactionAsync(DiscordEmoji.FromName(ctx.Client, ":mag:"));
+                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":cry:"));
+                return;
+            }
+
+            string _searchTerm = null;
+            if (args != null && args.Any()) _searchTerm = string.Join(' ', args);
+
+            var _url = _config["SCTM:Urls:Ships"];
+            if (_searchTerm != null) _url += $"?search={_searchTerm}";
+
+
+
+            var _request = await _client.GetAsync(_url);
+            var _content = await _request.Content.ReadAsStringAsync();
+
+            await ctx.Message.DeleteOwnReactionAsync(DiscordEmoji.FromName(ctx.Client, ":mag:"));
+
+            if (_request.IsSuccessStatusCode)
+            {
+                
+                var _ships = JsonConvert.DeserializeObject<List<Ship>>(_content);
+
+                foreach (var ship in _ships)
+                {
+                    var _embed = Embeds.Ship(ship, ctx.Client.CurrentUser);
+                    await ctx.Message.RespondAsync(null, false, _embed);
+                }
+            } else
+            {
+                Log.Error("{logAction}: unsuccessful API call: {@content}", _logAction, _content);
+                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":cry:"));
+                return;
+            }
+        }
+    }
+}
---
+++ sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Embeds/_Ship.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using sctm.connectors.rsi.models;
+using System;
+
+namespace sctm.services.discordBot
+{
+    public partial class Embeds
+    {
+        public static DiscordEmbed Ship(Ship ship, DiscordUser bot)
+        {
+            string _timeAgo = "";
+            if ((DateTime.Now - ship.LastRead).TotalMinutes < 1) _timeAgo = "a few seconds ago";
+            else if ((DateTime.Now - ship.LastRead).TotalMinutes >= 1 && (DateTime.Now - ship.LastRead).TotalMinutes <= 60) _timeAgo = Math.Round((DateTime.Now - ship.LastRead).TotalMinutes, 0) + " minutes ago";
+            else _timeAgo = Math.Round((DateTime.Now - ship.LastRead).TotalHours, 0) + " hours ago";
+
+            var _minCrew = 1;
+            int.TryParse(ship.CrewMin.ToString(), out _minCrew);
+
+            var _maxCrew = 1;
+            int.TryParse(ship.CrewMax.ToString(), out _maxCrew);
+
+            var _ret = new DiscordEmbedBuilder
+            {
+                Author = new DiscordEmbedBuilder.EmbedAuthor { IconUrl = ship.Manufacturer.LogoPath, Name = ship.Manufacturer.Name },
+                Title = $"{ship.ModelName} - *[{ship.State}]*",
+                Description = $"{ship.Description}  [RSI Link]({ship.RSILink})",
+                ImageUrl = ship.ThumbnailPath,
+                Color = (ship.State.ToLower() == "flight ready") ? DiscordColor.Green : DiscordColor.Red,
+                Footer = new DiscordEmbedBuilder.EmbedFooter { Text = $"Star Citizen Tools by SC TradeMasters - Read from RSI {_timeAgo}", IconUrl = bot.AvatarUrl }
+            }
+            .AddField($"{ship.Size}: {_minCrew}-{_maxCrew} crew", ship.Focus, true)
+            .AddField($"Cargo Capacity: {ship.CargoCapacity ?? 0}", $"{ship.Length ?? 0}x{ship.Beam ?? 0}x{ship.Height ?? 0} LBH", true)
+            ;
+
+
+            return _ret;
+        }
+    }
+}
