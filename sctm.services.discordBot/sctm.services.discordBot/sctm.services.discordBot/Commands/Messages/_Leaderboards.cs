a4e3c503a291c026d3206457d95faa8ff3f63d80
chrisipeters <chris.peters@menoko.com.au>
2020-09-20 18:12:13 +1000

    fix for no season running

--- sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_Leaderboards.cs
+++ sctm.services.discordBot/sctm.services.discordBot/sctm.services.discordBot/Commands/Messages/_Leaderboards.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using Serilog;
+using System;
 using System.Threading.Tasks;
 
 namespace sctm.services.discordBot.Commands.Messages
@@ -13,23 +15,37 @@ namespace sctm.services.discordBot.Commands.Messages
         [Aliases("leaders")] // alternative names for the command
         public async Task AddCommands_Leaderboards(CommandContext ctx, [Description("Leaderboard")] params string[] args)
         {
+            var _logAction = "AddCommands_Leaderboards";
             var _leaderboard = string.Join(' ', args);
             await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":mag:"));
 
             if (_leaderboard.Trim().ToLower() == "mining")
             {
-                var _data = await _services.GetLeaderboards_Organization(ctx.Guild.Id.ToString());
-                if(_data != null)
+                try
                 {
-                    var _embed = await Embeds.Leaderboard_Mining(ctx, ctx.Client.CurrentUser, _data);
+                    var _data = await _services.GetLeaderboards_Organization(ctx.Guild.Id.ToString());
+                    if (_data != null)
+                    {
+                        var _embed = await Embeds.Leaderboard_Mining(ctx, ctx.Client.CurrentUser, _data);
 
-                    
 
-                    await ctx.RespondAsync(null, false, _embed);
-                } else
-                {
-                    await ctx.Message.RespondAsync("There is no active season running - Previous season results will be available on our website soon");
+
+                        await ctx.RespondAsync(null, false, _embed);
+                    }
+                    else
+                    {
+                        await ctx.Message.RespondAsync("There is no active season running - Previous season results will be available on our website soon");
+                    }
                 }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "{logAction}: Error getting mining leaderboard data: {error}", _logAction, ex.Message);
+
+                    //await supportChannel.SendMessageAsync($"Error getting mining leaderboards: {ex.Message}");
+
+                    await ctx.Message.RespondAsync("I wasn't able to retrieve any active season data...");
+                }
+                
 
                 await ctx.Message.DeleteOwnReactionAsync(DiscordEmoji.FromName(ctx.Client, ":mag:"));
             }
